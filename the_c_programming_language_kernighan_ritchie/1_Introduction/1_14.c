91950e8cdf93fee3b76c93fda8bf594b49f62e27
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-01-31 14:07:41 +0100

    1.6 exercises

---
+++ the_c_programming_language_kernighan_ritchie/1_Introduction/1_14.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define IN  1
+#define OUT 0
+
+void main()
+{
+    int c;
+    int counters[26];
+
+    for (int i = 0; i < 26; ++i)
+        counters[i] = 0;
+
+    while ((c = getchar()) != EOF) 
+    {
+        if (c >= 'a' && c <= 'z')
+            counters[c-'a']++;
+    }
+    
+    // frequencies
+    printf("\nCharater historgram\n");
+    for (int i=0; i<26; i++)
+    {
+        printf("%c: ", 'a'+i);
+        for (int j=0; j<counters[i]; j++)
+            printf("-");
+        printf("\n");
+    }
+}
