77f2efdd8e729dece5fc5c6db96e849763a69843
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-07-17 11:44:02 +0200

    minor

--- the_c_programming_language/1_Introduction/examples/word_counting.c
+++ the_c_programming_language/1_Introduction/examples/word_counting.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define IN 1
+#define IN  1
 #define OUT 0
 
 void main()
@@ -8,6 +8,7 @@ void main()
     int c, nl, nw, nc, state;
     state = OUT;
     nl = nw = nc = 0;
+
     while ((c = getchar()) != EOF) 
     {
         ++nc;
