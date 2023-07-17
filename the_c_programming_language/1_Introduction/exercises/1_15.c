9952240d796944b51719d28bc8d74ed898b59f38
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-07-17 13:31:24 +0200

    change to while loop

--- the_c_programming_language/1_Introduction/exercises/1_15.c
+++ the_c_programming_language/1_Introduction/exercises/1_15.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-#define LOWER   0
-#define UPPER   300
-#define STEP    20
-
 float fahr_to_celsius(float fahr);
 
-void main()
+int main()
 {
-    for (int fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-        printf("%3d %6.1f\n", fahr, fahr_to_celsius(fahr));
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        printf("%3.0f %7.1f\n", fahr, fahr_to_celsius(fahr));
+        fahr += step;
+    }
+
     return 0;
 }
 
