813e1af43ff8bba2fb7fb1e874947f579dfe0f38
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-07-17 19:05:16 +0200

    rename files

--- the_c_programming_language/1_Introduction/examples/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-dir = os.getcwd()
-print(dir)
-files = os.listdir()
-for file in files:
-    new_name = file.replace("-", "_")
-    os.rename(f"{dir}\{file}", f"{dir}\{new_name}")
