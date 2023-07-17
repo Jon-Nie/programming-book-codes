8e8a92345285e9e5d329bdd4a595778d3b5ded30
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-07-17 13:31:34 +0200

    minor

--- the_c_programming_language/1_Introduction/examples/celsius_while.c
+++ the_c_programming_language/1_Introduction/examples/celsius_while.c
@@ -13,7 +13,7 @@ void main()
     while (fahr <= upper) 
     {
         celsius = 5.0/9.0 * (fahr-32.0);
-        printf("%10.0f\t%7.1f\n", fahr, celsius);
+        printf("%3.0f %7.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
